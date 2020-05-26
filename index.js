7f682647b2e1c8dc0fb30fb85e7d2baa802df64d
unknown <jeong214132@knu.ac.kr>
2020-05-26 17:40:30 +0900

    로그인 기능 만들기, 토큰 쿠키에 저장

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const {User} = require("./models/User")
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI
 ,{useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false})
@@ -25,6 +28,30 @@ app.post('/register', (req, res) => {
         if(err) return res.json({success:false, err})
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login',(req, res)=>{
+    User.findOne({email:req.body.email}, (err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "입력하신 이메일은 없는 이메일입니다."
+            })
+        }
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "입력하신 비밀번호가 일치하지 않습니다."
+                })
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie("x_auth",user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
         })
     })
 })
